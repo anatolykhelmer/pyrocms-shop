3e8a28ccc2f05990cb109f9644a5d0e2bfec7d6a
Eko Muhammad Isa <ekoisa@gmail.com>
2011-09-20 07:39:48 +0700

    add site_url

--- shop/views/index.php
+++ shop/views/index.php
@@ -1,7 +1,7 @@
 <h2><?php echo $this->module_details['name']; ?></h2>
 <ul><?php echo lang('shop.cat_list_title'); ?>
     <?php foreach($shop_categories->result() as $category) {
-        echo '<li><a href="/shop/view_category/' .$category->id. '" >' .$category->name. '</a></li>';
+        echo '<li><a href="'.site_url("/shop/view_category/".$category->id).'" >' .$category->name. '</a></li>';
     }
     ?>
 </ul>
